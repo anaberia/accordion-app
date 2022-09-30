da173515b1c62cee4dd1831db4437d8d6c136491
anaberia <ana.beria3@gmail.com>
2022-09-30 16:15:14 -0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    title: "Do I have to allow the use of cookies?",
+    info: "You do not have to accept cookies. Rules like the GDPR were designed to give you control over your data and browsing history. The potential problem with refusing to accept cookies is that some website owners may not allow you to use their websites if you do not accept their cookies. Another downside is that without acceptance, you may not receive the full user experience on certain websites.",
+  },
+  {
+    id: 2,
+    title: "How do I change my My Page password?",
+    info: "Open your Google Account. You might need to sign in. Under security, select Signing in to Google. Choose Password. You might need to sign in again. Enter your new password, then select Change Password.",
+  },
+  {
+    id: 3,
+    title: "What is BankID?",
+    info: "It is a personal and easy electronic credential for secure identification and signing online. In the physical world, you are identified through your passport, driver's license or bank card. In the online world, you can use your BankID to confirm who you are.",
+  },
+  {
+    id: 4,
+    title: "Whose birth number can I use?",
+    info: "Opposite-sex couples, Married or civil-partner parents. Either parent can register the birth on their own. They can include both parents’ details if they were married or in a civil partnership when the baby was born or conceived.",
+  },
+  {
+    id: 5,
+    title: "When do I recieve a password ordered by letter?",
+    info: "Head to the Gmail sign-in page and click the “Forgot Password” link. Enter the last password you remember. If you can't remember one, click “Try a different question.” Enter the secondary email address you used when you set up your Gmail account to get a password reset email.",
+  },
+];
+export default questions;
